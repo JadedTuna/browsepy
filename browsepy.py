4ed24a89300f4d8c76794a7d38f453e2e080cde2
Victor <digitloft@gmail.com>
2014-09-04 14:07:31 +0200

    Create browsepy.py

---
+++ browsepy.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+import ui
+import os
+import imp
+import json
+
+appsfn  = "apps.json"
+appsdir = "apps"
+
+if not os.path.exists(appsfn):
+    with open(appsfn, "w") as fp:
+        fp.write("{}")
+
+with open(appsfn) as fp:
+    appnames = json.load(fp)
+
+if not os.path.exists(appsdir):
+    os.mkdir(appsdir)
+
+apps = {}
+for name, exts in appnames.items():
+    path = os.path.join(os.getcwd(), appsdir, "{}".format(name))
+    App = imp.load_source("{}".format(name, path), path).App
+    apps[App] = exts
+
+class Delegate(object):
+    def __init__(self):
+        self.curpath = os.getcwd()
+    
+    def tableview_did_select(self, tableview, section, row):
+        # Called when a row was selected.
+        name = tableview.data_source.items[row]
+        abspath = os.path.abspath(os.path.join(self.curpath, name))
+        if name.endswith("/"):
+            _name = name[:-1]
+            if os.path.isdir(abspath):
+                lst = getDirListing(abspath)
+                if lst:
+                    self.curpath = abspath
+                    tableview.data_source.items = lst.items
+                    tableview.superview.name = os.path.split(self.curpath)[-1]
+        else:
+            if os.path.isfile(abspath):
+                _, ext = os.path.splitext(abspath)
+                for App, exts in apps.items():
+                    if ext in exts:
+                        return App(view, abspath)
+
+def getDirListing(curpath):
+    try:
+        all = os.listdir(curpath)
+    except OSError:
+        return
+    folders = [i + "/" for i in all if os.path.isdir(
+                                    os.path.join(curpath, i)
+                                    )]
+    files   = [i for i in all if os.path.isfile(
+                                    os.path.join(curpath, i)
+                                    )]
+    lst = ui.ListDataSource(["../"] + folders + files)
+    lst.font = ('Courier', 18)
+    return lst
+
+view = ui.View()
+
+table = ui.TableView()
+table.flex = "WH"
+table.delegate = Delegate()
+table.data_source = getDirListing(table.delegate.curpath)
+
+view.name = os.path.split(table.delegate.curpath)[-1]
+view.add_subview(table)
+
+view.present("sheet")
